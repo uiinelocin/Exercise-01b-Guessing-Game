6c4685db63fb59d946c4bac6b3ef0e1f7ee09f76
ninico <ninico@iu.edu>
2020-09-02 10:38:14 +0800

    Make my own guessing game by modifying main2.py

--- main2.py
+++ main2.py
@@ -1,2 +1,25 @@
+# Nicole Ni, MSCH-C220
 import sys, random
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+number = random.randrange(1,10)
+life = 5
+while life > 0:
+    guess = input("Guess a number from 1 to 10, you have " + str(life) + " chances to guess: ")
+    if guess.isnumeric() == True:
+        guess = int(guess)
+        if guess == number:
+            print("Great job! You got the cookie!")
+            print("The number was " + str(number))
+            break
+        elif guess > number:
+            print("Guess lower?")
+            life -= 1
+        elif guess < number:
+            print("Guess higher?")
+            life -= 1
+    else: print("Please enter a NUMBER!")
+else :
+    print("Aww! You ran out of your chances!")
+    print("The number was " + str(number))
+
+
